dbba5f0966f82ba655564e0476f76a3e5825e7d9
seisyun9665 <minecraft.syun0521@gmail.com>
2024-08-21 00:47:25 +0900

    feat: add retry script

---
+++ Assets/Script/RetryButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RetryButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+    }
+}
